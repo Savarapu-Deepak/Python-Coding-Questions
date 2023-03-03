6578146af02f48552bed761e55722e0e1dc3e209
Deepak <savarapudeepak9@gmail.com>
2023-03-03 18:05:01 +0530

    Common Characters in two strings - Using Functions also added.

--- Find out common characters in two lists.py
+++ Find out common characters in two lists.py
@@ -1,9 +1,17 @@
 # Python Program to print the common letters in two strings.
 
-data_1 = input('Enter Any String : ')
+'''data_1 = input('Enter Any String : ')
 data_2 = input('Enter Any String : ')
 
 x, y = set(data_1), set(data_2)
 
 result = x & y
-print(result)
+print(result)'''
+
+'''def common_Letters(s1, s2):
+    x, y = set(s1), set(s2)
+    result = x & y
+    print(result)
+s1 = input('Enter String_1 : ')
+s2 = input('Enter String_2 : ')
+common_Letters(s1, s2)'''
