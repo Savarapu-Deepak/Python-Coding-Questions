541c20e446dde549fc134f2c29e71b924a0d36a0
Deepak <savarapudeepak9@gmail.com>
2023-03-03 19:38:10 +0530

    Largest & Smallest in array

---
+++ Find Largest and Smallest Element in Array.py
@@ -0,0 +1,6 @@
+# Python Program to print largest and smallest element in array.
+
+data = [63, 54, 98, 34, 89, 42, 18]
+data.sort()
+print('Smallest Element :', data[0])
+print('Largest Element : ', data[-1])
